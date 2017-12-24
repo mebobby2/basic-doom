cd1d184191b5a9f48490a0c3e3b07822d484597f
Bobby Lei <bobl53@gmail.com>
2017-12-25 00:50:14 +0800

    enemy shoot fireball

--- BasicDoom/Assets/WanderingAI.cs
+++ BasicDoom/Assets/WanderingAI.cs
@@ -8,6 +8,10 @@ public class WanderingAI : MonoBehaviour {
 
 	private bool _alive;
 
+	[SerializeField]
+	private GameObject fireballPrefab;
+	private GameObject _fireball;
+
 	void Start() {
 		_alive = true;
 	}
@@ -20,7 +24,15 @@ public class WanderingAI : MonoBehaviour {
 			Ray ray = new Ray (transform.position, transform.forward);
 			RaycastHit hit;
 			if (Physics.SphereCast(ray, 0.75f, out hit)) {
-				if (hit.distance < obstableRange) {
+				GameObject hitObject = hit.transform.gameObject;
+				if (hitObject.GetComponent<PlayerCharacter>()) {
+					if (_fireball == null) {
+						_fireball = Instantiate (fireballPrefab) as GameObject;
+						_fireball.transform.position = transform.TransformPoint (Vector3.forward * 1.5f);
+						_fireball.transform.rotation = transform.rotation;
+					}
+				}
+				else if (hit.distance < obstableRange) {
 					float angle = Random.Range (-110, 110);
 					transform.Rotate (0, angle, 0);
 				}
